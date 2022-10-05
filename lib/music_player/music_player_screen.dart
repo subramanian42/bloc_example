03fdfe51550207651b7b55e283aeaea9e6d64de6
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2022-10-05 20:41:50 +0530

    code Refactoring: - provided isplaying boolean to the media controls and albumart

--- lib/music_player/music_player_screen.dart
+++ lib/music_player/music_player_screen.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc/music_player_bloc.dart';
 import 'widgets/album_art.dart';
-import 'widgets/album_title.dart';
+// import 'widgets/album_title.dart';
 import 'widgets/media_controls.dart';
-import 'widgets/progress_bar.dart';
+// import 'widgets/progress_bar.dart';
 
 class MusicPlayerScreen extends StatelessWidget {
   const MusicPlayerScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
+    return BlocBuilder<MusicPlayerBloc, MusicPlayerState>(
+      builder: (context, state) {
+        return Scaffold(
+          body: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              AlbumArt(
+                image: 'assets/images/track_image_1.jpeg',
+                isPlaying: state.isPlaying,
+              ),
+              //  const AlbumTitle(),
+              // const ProgressBar(),
+              const SizedBox(
+                height: 50,
+              ),
+              MediaControls(isplaying: state.isPlaying),
+            ],
           ),
-          const AlbumArt(),
-          //  const AlbumTitle(),
-          // const ProgressBar(),
-          const SizedBox(
-            height: 50,
-          ),
-          MediaControls(),
-        ],
-      ),
+        );
+      },
     );
   }
 }
